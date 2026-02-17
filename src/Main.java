6cef787969378a096fc598e11fc1757c79241a1b
V1C20R <sm9502@srmist.edu.in>
2026-02-17 10:57:01 +0530

    UC1

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,12 @@
-import java.util.Scanner;
-
 class check_palindrome {
-    boolean isPalindrome(int number) {
-        String n = String.valueOf(number);
-        int f = 0;
-        int l = n.length() - 1;
-        int count = 0;
-        while (f < l) {
-            if (n.charAt(f) != n.charAt(l)) {
-                return false;
-            }
-            f++;
-            l--;
-        }
-
-        return true;
+    void Greet(){
+        System.out.println("Welcome to palindrome checker Managment System");
     }
 }
 
 public class Main {
     public static void main(String[] args){
         check_palindrome c = new check_palindrome();
-        Scanner input = new Scanner(System.in);
-        System.out.println("Enter number: ");
-        int n = input.nextInt();
-        System.out.println(c.isPalindrome(n) ? "Yes" : "No");
+        c.Greet();
     }
 }
