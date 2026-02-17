8db9a4ef2b0c649cc42ec89c9ea5ee9f8e300d02
V1C20R <sm9502@srmist.edu.in>
2026-02-17 10:51:41 +0530

    UC2

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,28 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+import java.util.Scanner;
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+class PalindromeChecker{
+    boolean isPalindrome(String str) {
+        int f = 0;
+        int l = str.length() - 1;
+        int count = 0;
+        while (f < l) {
+            if (str.charAt(f) != str.charAt(l)) {
+                return false;
+            }
+            f++;
+            l--;
+        }
+
+        return true;
+    }
+}
+
+public class Main {
+    public static void main(String[] args){
+        PalindromeChecker c = new PalindromeChecker();
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter String: ");
+        String str = input.nextLine();
+        System.out.println(c.isPalindrome(str) ? "Yes" : "No");
     }
 }
