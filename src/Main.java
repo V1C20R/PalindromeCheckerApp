e63e322b96dcc9e0da544cbf267a7c2eaabeb85f
V1C20R <sm9502@srmist.edu.in>
2026-03-11 09:40:34 +0530

    Use Case 6

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,36 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+import java.util.Scanner;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+class PalindromeChecker{
+    boolean isPalindrome(String str){
+        Queue<Character> q = new LinkedList<>();
+        Stack<Character> s = new Stack<>();
+        for(int i=0;i<str.length();i++){
+            q.offer(str.charAt(i));
+            s.push(str.charAt(i));
+        }
+
+        for(int i=0;i<str.length();i++){
+            if(q.poll() != s.pop()){
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
+
+class Main{
+    public static void main(String[] args){
+        PalindromeChecker c = new PalindromeChecker();
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter String: ");
+        String str = input.nextLine();
+
+        System.out.println("Is it a palindrome string?");
+        System.out.println(c.isPalindrome(str) ? "Yes" : "No");
     }
 }
